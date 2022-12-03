4ea2bc713de525f9a0c799f8554d32017e879c4f
kimbifavour <kimbifavour@gmail.com>
2022-12-03 07:33:52 +0100

    More exercises done

--- 4_ChapterFour/ChapterFour.cs
+++ 4_ChapterFour/ChapterFour.cs
@@ -9,7 +9,13 @@ class Run{
         // ChapterFour.Three();
         // ChapterFour.Four();
         // ChapterFour.Five();
-        ChapterFour.Six();
+        // ChapterFour.Six();
+        // ChapterFour.Seven();
+        // ChapterFour.Eight();
+        // ChapterFour.Nine();
+        // ChapterFour.Ten();
+        // ChapterFour.Eleven();
+        // ChapterFour.Twelve(); 
     }
 }
 
@@ -43,17 +49,17 @@ class Run{
     }
 
         
-    // public class Try{
-    //     public static void TryParse(){
-    //         String str = Console.ReadLine();
-    //         int intValue;
-    //         bool parseSuccess = Int32.TryParse(str, out intValue); //Parameters: a parsing string and a variable to store the result of successful parsing
-    //         Console.WriteLine(parseSuccess? "The Square of the number is : " + (intValue * intValue) : "Invalid number");
+    public class Try{
+        public static void TryParse(){
+            String? str = Console.ReadLine();
+            int intValue;
+            bool parseSuccess = Int32.TryParse(str, out intValue); //Parameters: a parsing string and a variable to store the result of successful parsing
+            Console.WriteLine(parseSuccess? "The Square of the number is : " + (intValue * intValue) : "Invalid number");
 
-    // //Note: The TryParse() method simultaneously returns two values, the bool result and the parsed value. 
-    // //This is possible because one of the values is returned as an output parameter represented (out parameter_name)
-    //     }
-    // }
+    //Note: The TryParse() method simultaneously returns two values, the bool result and the parsed value. 
+    //This is possible because one of the values is returned as an output parameter represented (out parameter_name)
+        }
+    }
 
     public class ReadKeyMethod{
         public static void ReadKey(){
@@ -159,7 +165,98 @@ class Run{
             int num1 = 5;
             int num2 = 3;
             int greater = Math.Max(num1, num2);
-            Console.WriteLine("The greater of the two numbers {0} and {1} is {2}", num1, num2, greater);
+            Console.WriteLine("The greater of the two numbers, {0} and {1} is {2}", num1, num2, greater);
+        }
+
+        public static void Seven(){
+            Console.WriteLine("\nSeven: Sum of five integers");
+            Console.WriteLine("Enter five integers: ");
+            int sum = 0;
+            int[] numbers = new int[5];
+            for(int i = 0; i<5; i++){
+                Console.Write("=>");
+                numbers[i] = int.Parse(Console.ReadLine());
+
+                // Type type = numbers[i].GetType();
+                // while(type != typeof(int)){
+                //     Console.WriteLine("Enter an Integer: ");
+                //     Console.Write("=>");
+                //     numbers[i] = int.Parse(Console.ReadLine());
+                // }
+                    sum = sum + numbers[i];
+            }
+
+            Console.WriteLine("Their sum is {0}", sum);
+        }
+
+        public static void Eight(){
+            Console.WriteLine("\nEight: Greatest of five numbers");
+            Console.WriteLine("Enter five integers: ");
+            int[] numbers = new int[5];
+            for(int i = 0; i<5; i++){
+                Console.Write("=>");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            int greater = 0;
+            for(int i = 0; i<5; i++){
+                greater = Math.Max(greater, numbers[i]);
+            }
+            Console.WriteLine("The greatest is {0}", greater);
+        }
+
+        public static void Nine(){
+            Console.WriteLine("\nNine: Sum of n number of numbers");
+            Console.Write("Enter the number of numbers, n: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter these {0} numbers: ", n);
+            int[] numbers = new int[n];
+            int sum = 0;
+
+            for(int i = 0; i<n; i++){
+                Console.Write("=>");
+                numbers[i] = int.Parse(Console.ReadLine());
+                sum += numbers[i];
+            }
+
+            Console.WriteLine("Sum: {0}", sum);
+
+        }
+
+        public static void Ten(){
+            Console.WriteLine("\nTen: Prints numbers in the range 1 to n");
+            Console.Write("Enter the last number in the range: ");
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i<=number; i++){
+                Console.Write(i + ", ");
+            }
+        }
+
+        public static void Eleven(){
+            Console.WriteLine("\nEleven: Fibonacci Series");
+            Console.WriteLine("First 50 numbers in the fibonacci series");
+            long first = 0;
+            long second = 1;
+            Console.Write("{0}, {1}, ", first, second);
+            for(int i = 0; i<48; i++){
+                long next = first + second;
+                Console.Write("{0}, ", next);
+                first = second;
+                second = next;
+            }
+            
+        }
+
+        public static void Twelve(){
+            Console.WriteLine("\nTwelve: Sum of fractional sequence.");
+            Console.Write("How many terms should be in the sequence: ");
+            float n = float.Parse(Console.ReadLine());
+            float sum = 1F;
+            for (float i = 2; i<n+1; i++){
+                sum = sum + (1/i);
+            }
+                Console.Write("{0 :F3}",sum);
         }
     }
 
