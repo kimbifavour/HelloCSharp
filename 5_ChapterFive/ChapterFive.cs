9e4b04b8a1934d6fa1d1f7d2b3736b7d265f206e
kimbifavour <kimbifavour@gmail.com>
2022-12-04 20:58:47 +0100

    minor changes

---
+++ 5_ChapterFive/ChapterFive.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 5;
+int b = a;
+a = 5*2;
+
+string greet = "hello";
+string change = greet;
+greet = "Nice";
+Console.WriteLine(greet);
+Console.WriteLine(change);
+
+Console.WriteLine(a);
+Console.WriteLine(b);
