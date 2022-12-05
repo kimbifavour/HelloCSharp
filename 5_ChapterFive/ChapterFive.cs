58d72305af2fb70a3d9c3f7c8407bf0c6d771066
kimbifavour <kimbifavour@gmail.com>
2022-12-05 20:08:28 +0100

    solved: one and two

--- 5_ChapterFive/ChapterFive.cs
+++ 5_ChapterFive/ChapterFive.cs
@@ -1,13 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-int a = 5;
-int b = a;
-a = 5*2;
+﻿using System;
 
-string greet = "hello";
-string change = greet;
-greet = "Nice";
-Console.WriteLine(greet);
-Console.WriteLine(change);
+class Run{
+    public static void Main(){
+        // ChapterFive.One();
+        ChapterFive.Two();
 
-Console.WriteLine(a);
-Console.WriteLine(b);
+    }
+}
+
+class ChapterFive{
+
+    public static void One(){
+        //an if-statement that swaps the values of two variables if the first is greater than the second
+        Console.WriteLine(".........................\nOne: Exchange numbers? ");
+        Console.Write("Enter the first number: ");
+        int greater = int.Parse(Console.ReadLine());
+        Console.Write("Enter the second number: ");
+        int lesser = int.Parse(Console.ReadLine());
+
+        if(lesser > greater){
+            int temp = greater;
+            greater = lesser;
+            lesser = temp;
+        }
+            Console.WriteLine("Greater: {0}\nLesser: {1}", greater, lesser);
+
+    }
+
+    
+    public static void Two(){
+        Console.WriteLine("........................\nTwo: Sign of the product");
+        Console.WriteLine("Enter three integers");
+        int num1 = int.Parse(Console.ReadLine());
+        int num2 = int.Parse(Console.ReadLine());
+        int num3 = int.Parse(Console.ReadLine());
+
+        if(num1 > 0 &&(num2<0 || num3<0)){
+            Console.WriteLine("Sign of product: -");
+        }
+
+        else if ((num1 < 0 && num2<0 && num3<0) || (num1>0 && num2>0 && num3>0)){
+            Console.WriteLine("Sign of product: +");
+        }
+    }
+
+
+    public static void Three(){
+        
+        Console.WriteLine("........................\nTwo: Sign of the product");
+    }
+}
