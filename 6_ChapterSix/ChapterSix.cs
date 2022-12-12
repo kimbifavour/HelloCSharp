4199d6322ea996b48f7a91819aba937dd95ba5db
kimbifavour <kimbifavour@gmail.com>
2022-12-12 21:06:19 +0100

    new: Chapter six

---
+++ 6_ChapterSix/ChapterSix.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Run{
+    public static void Main(){
+
+        ChapterSix.One();
+    }
+
+}
+
+class ChapterSix{
+
+    public static void One(){
+        //Prints numbers from one to N
+        Console.WriteLine(".........................\nOne: Print numbers one to N");
+        Console.Write("Enter a number N: ");
+        int n = int.Parse(Console.ReadLine());
+        int i = 1;
+
+        while(i<=n){
+            Console.Write(i);
+            Console.Write(", ");
+            i++;
+        }
+    }
+}
